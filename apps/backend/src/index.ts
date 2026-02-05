29289bdb6800477f90adb105a169be56924b909d
kussy-tessy <45926182+kussy-tessy@users.noreply.github.com>
2026-02-05 22:39:17 +0900

    fix: 404の扱いが本番環境で意図通りに動いていない問題を修正

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -15,10 +15,6 @@ app.register(fastifyCors, {
 	origin: ["http://localhost:5173", "https://off.kg-misskey.net"],
 });
 
-app.get("/", (_, reply) => {
-	reply.sendFile("index.html");
-});
-
 app.get("/api/hello", async () => {
 	return prisma.user.findUnique({ where: { id: "1" } });
 });
@@ -30,7 +26,14 @@ app.register(fastifyStatic, {
 	prefix: "/",
 });
 
-console.log("[BOOT] DATABASE_URL =", process.env.DATABASE_URL);
+app.setNotFoundHandler((req, reply) => {
+	if (req.raw.url?.startsWith("/api/")) {
+		reply.code(404).send({ message: "API route not found" });
+		return;
+	}
+	reply.sendFile("index.html");
+});
+
 app.listen({ port: 3000, host: "0.0.0.0" });
 
 app.setErrorHandler((err, _req, _reply) => {
